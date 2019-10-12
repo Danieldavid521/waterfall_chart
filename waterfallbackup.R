e4781a3fb95560c39811e59292eaf01d23902e37
daniel <daniel@MBP-12.local>
2019-10-12 07:44:47 -0500

    changed code

--- waterfallbackup.R
+++ waterfallbackup.R
@@ -1,32 +1,32 @@
-#getwd()
+
 library(ggplot2)
 
-desc <- c("loan1", "loan2", "loan3", "loan4", "loan5", "loan6")
-#amount <- read.csv("waterfall.csv", header = TRUE)
-#as.vector(amount)
+desc <- c("Pay day", "ATM", "ATM Fees", "Savings", "Centennial Lanes", "Owed Dalton $20", "Dominos", "Isaac gave me $10", "Dillions Groceries", "H20 Float", "Tiger Burger")
+amount <- read.csv("waterfall.csv", header = TRUE)
+as.vector(amount)
 
-type <- c("Outcome", "Outcome", "Outcome", "Outcome", "Outcome", "Outcome")
-
-
-start <- c(0, 5500.00, 11000.00, 12851.00, 1452.28, 1452.28)
-
-end <- c( 5500.00,  11000.00 ,  12851.00 , 1452.28, 1458.13, 1478.13)
-
-is_income <- c(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)
-
-ref <- c(5500.00, 5500.00,  1851.00 ,  3250.00,  7500,7500,5500)
-
-account <- data.frame(desc, amount, type, start, end, is_income, ref, 1:6)
+type <- c("Income", "Outcome", "Outcome", "Outcome", "Outcome", "Outcome", "Outcome", "Income", "Outcome", "Outcome", "Outcome")
 
 
+start <- c(0, 1227.78, 1205.78, 1203.28, 1003.28, 997.43, 977.43, 941.56, 951.56, 903.55, 843.55 )
 
-account$ID <- account$X1.6
+end <- c(1227.78, 1205.78, 1203.28, 1003.28, 997.43, 977.43, 941.56, 951.56, 903.55, 843.55, 833.77)
+
+is_income <- c(TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE)
+
+ref <- c(1227.78, -22, -2.5, -200.00, -5.85, -20, -35.87, 10, -48.01, -60, -9.78)
+
+account <- data.frame(desc, amount, type, start, end, is_income, ref, 1:11)
+
+
+
+account$ID <- account$X1.11
 account$X1.11 <- NULL 
 
 account$type <- as.factor(account$type)
 
-ggplot(account, aes(desc, fill = type)) + geom_rect(aes(x = desc, xmin = ID - 0.45, xmax = ID + .45, ymin = end, ymax = start)) + scale_fill_manual(values = c("red","green"))
-
+ggplot(account, aes(desc, fill = type)) + geom_rect(aes(x = desc, xmin = ID - 0.45, xmax = ID + .45, ymin = end, ymax = start)) + scale_fill_manual(values = c("green","red"))
++ theme_dark()
 
 #p1 <- ggplot(account, aes(x = ID,y = ref, fill=is_income)) +
  #     geom_bar(stat="identity", position="identity") +  scale_fill_manual(values = c("red","green")) + theme_dark()
